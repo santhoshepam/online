1344fe5fd8c5055f8d4b4d188cec7d61b67e1df7
santhoshepam <santhosh_katakam@epam.com>
2017-04-17 20:27:36 +0530

    Add files via upload

---
+++ Cricketer.java
@@ -0,0 +1,25 @@
+package programs;
+
+public class Cricketer {
+
+	String name="default value";
+	public static void main(String[] args) {
+		
+		Cricketer cricketer=new Cricketer();
+		Cricketer newer= new Cricketer();
+		cricketer.name="Sacfffhin";
+		newer.name="ponkkkkkkkkkkkkkiting";
+		cricketer.new1();
+		newer.new2();
+		
+		
+		}
+	public void new1(){
+		System.out.println(name+"san");
+	}
+		public void new2(){
+			System.out.println(name+"ksany");
+		}
+		}
+	
+
